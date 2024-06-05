9b677cf9a235f8884e0357a576a12a7f9a2e04f6
MathPelicer <matheuspelicer58@gmail.com>
2024-06-05 15:23:04 -0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,12 +7,18 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	//fmt.Println("Logs from your program will appear here!")
-
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		panic(err)
+	}
+
+	if input == "nonexistent\n" {
+		fmt.Fprint(os.Stdout, "nonexistent: command not found\n")
+	}
+
 }
