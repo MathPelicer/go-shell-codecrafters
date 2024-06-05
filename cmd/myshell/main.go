05798a595c0b3edfa033af3564004d305e4ddfb4
MathPelicer <matheuspelicer58@gmail.com>
2024-06-05 18:06:29 -0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,9 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+func getImplementedCommands() []string {
+	return []string{"exit", "echo", "type"}
+}
 
 func handleInputs(input string) {
 	inputCommand := strings.SplitN(strings.Trim(input, "\n"), " ", 2)
@@ -14,8 +19,18 @@ func handleInputs(input string) {
 	case "exit":
 		converted, _ := strconv.Atoi(inputCommand[1])
 		os.Exit(converted)
+		return
 	case "echo":
 		fmt.Fprintf(os.Stdout, "%s\n", inputCommand[1])
+		return
+	case "type":
+		implementedCommands := getImplementedCommands()
+		if slices.Contains(implementedCommands, inputCommand[1]) {
+			fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", inputCommand[1])
+			return
+		}
+		fmt.Fprintf(os.Stdout, "%s not found\n", inputCommand[1])
+
 	default:
 		fmt.Fprintf(os.Stdout, "%s: command not found\n", inputCommand[0])
 	}
