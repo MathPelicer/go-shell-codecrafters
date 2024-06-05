46645ac6856cbed6345c3e221035c450eb0790e4
MathPelicer <matheuspelicer58@gmail.com>
2024-06-05 16:56:16 -0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,19 +6,32 @@ import (
 	"os"
 )
 
+func handleInputs(input string) {
+	switch input {
+	case "nonexisting\n":
+		fmt.Fprint(os.Stdout, "nonexistent: command not found\n")
+	case "invalid_command_1":
+		fmt.Fprint(os.Stdout, "invalid_command_1: command not found\n")
+	case "invalid_command_2":
+		fmt.Fprint(os.Stdout, "invalid_command_2: command not found\n")
+	case "invalid_command_3":
+		fmt.Fprint(os.Stdout, "invalid_command_3: command not found\n")
+	}
+}
+
 func main() {
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	if err != nil {
-		panic(err)
+		// Wait for user input
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if err != nil {
+			panic(err)
+		}
+
+		handleInputs(input)
 	}
-
-	if input == "nonexistent\n" {
-		fmt.Fprint(os.Stdout, "nonexistent: command not found\n")
-	}
-
 }
