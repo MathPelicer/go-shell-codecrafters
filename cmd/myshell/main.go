847ac7f589cf1cbfa523d7cd6349fdb72dcf765f
MathPelicer <matheuspelicer58@gmail.com>
2024-06-05 20:26:42 -0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"slices"
 	"strconv"
 	"strings"
@@ -13,24 +15,46 @@ func getImplementedCommands() []string {
 	return []string{"exit", "echo", "type"}
 }
 
-func handleInputs(input string) {
+func seachExecutable(executable string) (string, error) {
+	paths := strings.Split(os.Getenv("PATH"), ":")
+
+	for _, path := range paths {
+		filePath := filepath.Join(path, executable)
+		_, err := os.Stat(filePath)
+		if err == nil {
+			return filePath, nil
+		}
+	}
+
+	newError := errors.New("file not found")
+
+	return "", newError
+}
+
+func handleInputCommands(input string) {
 	inputCommand := strings.SplitN(strings.Trim(input, "\n"), " ", 2)
 	switch inputCommand[0] {
 	case "exit":
 		converted, _ := strconv.Atoi(inputCommand[1])
 		os.Exit(converted)
-		return
 	case "echo":
 		fmt.Fprintf(os.Stdout, "%s\n", inputCommand[1])
-		return
 	case "type":
 		implementedCommands := getImplementedCommands()
+
 		if slices.Contains(implementedCommands, inputCommand[1]) {
 			fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", inputCommand[1])
 			return
 		}
-		fmt.Fprintf(os.Stdout, "%s not found\n", inputCommand[1])
 
+		executablePath, err := seachExecutable(inputCommand[1])
+
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "%s not found\n", inputCommand[1])
+			return
+		}
+
+		fmt.Fprintf(os.Stdout, "%s is %s\n", inputCommand[1], executablePath)
 	default:
 		fmt.Fprintf(os.Stdout, "%s: command not found\n", inputCommand[0])
 	}
@@ -49,6 +73,6 @@ func main() {
 			panic(err)
 		}
 
-		handleInputs(input)
+		handleInputCommands(input)
 	}
 }
